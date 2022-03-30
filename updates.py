d305c6e1d2450558d77350cfae12ecd8658ac562
Stefano Bianchi <79590448+Stefano314@users.noreply.github.com>
2022-03-30 17:06:39 +0200

    Added kegg_script() function.
    This is still to be developed, it will become the function to build from scratches the full structure of the dataframes.

--- updates.py
+++ updates.py
@@ -14,3 +14,31 @@ def kegg_update(kegg_df, info):
         result_futures = [f1, f2, f3, f4]
         results = [f.result() for f in concurrent.futures.as_completed(result_futures)]
         return [element for elements in results for element in elements]
+    
+    def kegg_script():
+    db_path = "./Databases/Kegg"
+    kegg_genomes = pd.read_csv("https://rest.kegg.jp/list/genome", sep = "(?<=\d)\t", engine = "python", header = None,
+                                        names=["KeggID", "Description"])
+    kegg_diseases = pd.read_csv("https://rest.kegg.jp/list/disease", sep = "(?<=\d)\t", engine = "python", header = None,
+                               names = ["KeggID", "Description"])
+    kegg_drugs = pd.read_csv("https://rest.kegg.jp/list/drug", sep = "(?<=\d)\t", engine = "python", header = None,
+                               names = ["KeggID", "Description"])
+    # kegg_genomes.to_csv(db_path+'kegg_genomes.csv')
+    # kegg_diseases.to_csv(db_path+'kegg_diseases.csv')
+    # kegg_drugs.to_csv(db_path+'kegg_drugs.csv')
+    gen = kegg_update(kegg_genomes, 'ref')
+    dis = kegg_update(kegg_diseases, 'ref')
+    dru = kegg_update(kegg_drugs, 'ref')
+
+    adjacency_gen = ['dummy', 'dummy']
+    adjacency_dis = ['dummy', 'dummy']
+    adjacency_dru = ['dummy', 'dummy']
+    for array1, array2, array3 in zip(gen, dis, dru):
+        adjacency_gen = np.vstack([adjacency_gen, array1])
+        adjacency_dis = np.vstack([adjacency_dis, array2])
+        adjacency_dru = np.vstack([adjacency_dru, array3])
+
+    adjacency_gen = np.delete(adjacency_gen, (0), axis = 0)
+    adjacency_dis = np.delete(adjacency_dis, (0), axis = 0)
+    adjacency_dru = np.delete(adjacency_dru, (0), axis = 0)
+    
